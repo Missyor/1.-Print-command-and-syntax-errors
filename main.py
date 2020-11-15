21d2b8c9197a3fc887d264aadffee1327296f831
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 15:07:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello world")
+Print("hello world")
+#####syntax error tell us which line the error is on and what type of error it is 
+
+#indents cause an error 
+
+#forgetting brackets or quotation marks cause an error 
+
